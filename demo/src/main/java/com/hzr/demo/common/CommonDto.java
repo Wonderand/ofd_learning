35c398c11a7b11eb71bda1f3d3c2b61c7c413a8a
hzr <2290641663@qq.com>
2023-12-11 18:18:25 +0800

    签章测试转移

---
+++ demo/src/main/java/com/hzr/demo/common/CommonDto.java
@@ -0,0 +1,1 @@
+package com.hzr.demo.common;import lombok.Data;/** * @Classname CommonDto * @Author: hzr * @Description TODO * @Version 1.0.0 * @Date 2023/12/11 17:53 * @Created by 22906 */@Datapublic class CommonDto {    private String id;    private Integer page;}
